1d6c910ebc1e61f971d3c6b5a82b2995cbb5b0c7
Lazar Nagulov <lazarnagulov1@gmail.com>
2023-12-26 22:35:45 +0100

    [test] Add optional argument for -test

--- Sudoku/Main.cpp
+++ Sudoku/Main.cpp
@@ -16,22 +16,27 @@ void Usage() {
 	std::cerr << "./Sudoku <input.txt> <output.txt>" << std::endl;
 	std::cerr << "    input.txt   - Read puzzle from this file" << std::endl;
 	std::cerr << "    output.txt  - Write puzzles into this file" << std::endl;
-	std::cerr << "./Sudoku -test" << std::endl;
+	std::cerr << "./Sudoku -test [test_folder]" << std::endl;
 }
 
 int main(int argc, char** argv) {
 	if (strcmp(argv[1], "-test") == 0) {
 		std::cout << "Testing:" << std::endl;
-		TestRun();
+		if (argc == 3) {
+			TestRun(std::string(argv[2]));
+		}
+		else {
+			TestRun("../../Testovi");
+		}
 		return 0;
 	}
 	if (argc != 3) {
 		Usage();
 		return 1;
 	}
+
 	std::string input = argv[1];
 	std::string output = argv[2];
-
 	if (!std::filesystem::exists(input)) {
 		std::cerr << "Error: Input file path " << input << " is invalid." << std::endl;
 		Usage();
@@ -41,6 +46,7 @@ int main(int argc, char** argv) {
 		std::cerr << "Error: Output file path " << output << " is invalid." << std::endl;
 		return 1;
 	}
+
 	Sudoku s(input, output);
 	s.Run();
 }
